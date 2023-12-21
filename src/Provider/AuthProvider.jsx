6868aa911a45e1f3a204cda540411252806ef90b
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-12-21 13:35:07 +0600

    setup Auth Provider related  code

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,68 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut, updateProfile, onAuthStateChanged } from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
 export const AuthContext = createContext(null)
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
 const AuthProvider = ({ children }) => {
-    
 
-    const authInfo={
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    // ------first time create user by email and password ----------------
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
+    // ---------- user sign in by email and password
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // ---------- google sign in -----------
+    const googleSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    // --------------user logout ----------------
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    //  --------- update user--------------
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        })
+    }
+
+    // ----------- user  onAuthStateChange-------------------
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log('CurrentUser-->', currentUser)
+            setLoading(false)
+        })
+        return () => {
+            return unsubscribe()
+        }
+    }, [])
+
+
+
+    //-----------------  all auth information-----------------
+    const authInfo = {
+        createUser,
+        signIn,
+        googleSignIn,
+        logout,
+        updateUserProfile,
+        user,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
