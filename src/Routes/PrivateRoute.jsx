9aaaa8d1a370273bd83ed77895ad5e489e415119
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-12-22 17:54:12 +0600

    added private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/prop-types */
+import { Navigate, useLocation } from "react-router-dom";
+import Loader from "../components/Shared/Loader";
+import useAuth from "../hooks/useAuth";
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+const PrivateRoute = ({ children }) => {
+    const location = useLocation()
+    const { user, loading } = useAuth()
+    if (loading) return <Loader />
+    if (user) return children
+    return <Navigate to='/login' state={{ from: location }} replace />
+
+
 };
 
 export default PrivateRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/routes'
 import AuthProvider from './Provider/AuthProvider'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <AuthProvider>
-   <RouterProvider router={router} />
+   <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
+
+       
+      </QueryClientProvider>
    </AuthProvider>
   </React.StrictMode>,
 )
